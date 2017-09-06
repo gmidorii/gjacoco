199fbfbd3d6a1a3f20b875a46c89b22f49b18329
tagasouichiro <stitow14@gmail.com>
2017-09-06 22:24:27 +0900

    Add blacklist

---
+++ blacklist.go
@@ -0,0 +1,36 @@
+package main
+
+import "golang/src/github.com/BurntSushi/toml"
+
+// BlackList is black list setting is not measuring coverage
+type BlackList struct {
+	Packages []string
+	Classes  []string
+}
+
+func (b *BlackList) hasPackage(pack string) bool {
+	for _, v := range b.Packages {
+		if pack == v {
+			return true
+		}
+	}
+	return false
+}
+
+func (b *BlackList) hasClass(class string) bool {
+	for _, v := range b.Classes {
+		if class == v {
+			return true
+		}
+	}
+	return false
+}
+
+func createBlackList(fpath string) BlackList {
+	var blackList BlackList
+	_, err := toml.DecodeFile(fpath, &blackList)
+	if err != nil {
+		return BlackList{}
+	}
+	return blackList
+}
